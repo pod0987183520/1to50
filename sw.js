bee38edc8d11ea827be5cc1d9b98bf942ae5761d
a0987183520 <a0987183520@gmail.com>
2026-08-27 16:26:39 +0800

    feat: [v5.04] 徹底消除5x5上下過大空白、底端三行文字同等字級且不折行

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = '1to50-pwa-cache-v5.03';
+const CACHE_NAME = '1to50-pwa-cache-v5.04';
 
 const ASSETS_TO_CACHE = [
   'index.html',
-  'style.css?v=5.03',
-  'game.js?v=5.03',
+  'style.css?v=5.04',
+  'game.js?v=5.04',
   'manifest.json?v=5.00',
   'z_img_app_192.png?v=5.00',
   'z_img_app_512.png?v=5.00',
